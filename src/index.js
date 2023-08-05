806bd67b4f8ee3e525237e7787aa4961eb54b47c
dark-han-b <xxlabbesxx@gmail.com>
2023-08-05 16:28:46 +0600

    HTML with React

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './scss/app.scss';
+
+import App from './App';
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render( // строгий режим strictmode
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
